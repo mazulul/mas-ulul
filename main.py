7f8460fae86b0d7b0ab69bf58b2e51ca374cd4c8
mazulul <128021811+mazulul@users.noreply.github.com>
2023-03-21 18:48:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, ulul!</p>"
+  return "<p>Nama : Muhammad ulul albab</p><p> Tempat,Tanggal Lahir : Malang, 31 Juli 2004</p><p> Alamat : dinoyo, lowokwaru, Malang</p><p> Jenis Kelamin : Laki-Laki</p><p> Kewarganegaraan : Indonesia</p><p> Agama : Islam</p>"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
