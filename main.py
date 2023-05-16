66afbc24ca0ba0ac9af3b0715e88022058cb7ebd
mazulul <128021811+mazulul@users.noreply.github.com>
2023-05-16 07:16:29 +0000

    Praktikum 2. Membuat Aplikasi Catatan Harian dengan Python Dash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output, State
 
 # Membuat instance Dash
 app = dash.Dash(__name__)
@@ -8,24 +9,59 @@ app = dash.Dash(__name__)
 # Layout aplikasi
 app.layout = html.Div(
     children=[
-        html.H1("Selamat Datang di Aplikasi Web Sederhana"),
-        dcc.Input(id="input-text", type="text", placeholder="Masukkan teks"),
-        html.Button("Kirim", id="submit-button", n_clicks=0),
-        html.Div(id="output")
-    ]
+        html.H1("Aplikasi Catatan Harian"),
+        html.Div(
+            children=[
+                dcc.Textarea(
+                    id="note-input",
+                    placeholder="Masukkan catatan di sini",
+                    style={"width": "100%", "height": "100px"}
+                ),
+                html.Button("Simpan", id="save-button", n_clicks=0),
+            ],
+            style={"margin-bottom": "20px"}
+        ),
+        html.Div(id="message"),
+        html.H2("Daftar Catatan Harian"),
+        html.Ul(id="note-list"),
+    ],
+    style={"max-width": "600px", "margin": "auto"}
 )
 
-# Callback untuk merespon input pengguna
+# Callback untuk menyimpan catatan harian
 @app.callback(
-    dash.dependencies.Output("output", "children"),
-    [dash.dependencies.Input("submit-button", "n_clicks")],
-    [dash.dependencies.State("input-text", "value")]
+    Output("message", "children"),
+    [Input("save-button", "n_clicks")],
+    [State("note-input", "value")]
 )
-def update_output(n_clicks, input_text):
-    if n_clicks > 0 and input_text:
-        return html.H2(f"Anda mengirim: {input_text}")
+def save_note(n_clicks, note):
+    if n_clicks > 0 and note:
+        # Simpan catatan ke basis data atau tempat penyimpanan lainnya
+        # Di sini, hanya mencetak catatan ke konsol sebagai contoh
+        print("Catatan baru:", note)
+        return html.P("Catatan berhasil disimpan!")
     else:
         return ""
-#menjalankan aplikasi
+
+# Callback untuk menampilkan daftar catatan harian
+@app.callback(
+    Output("note-list", "children"),
+    [Input("save-button", "n_clicks")]
+)
+def display_notes(n_clicks):
+    if n_clicks > 0:
+        # Ambil daftar catatan dari basis data atau tempat penyimpanan lainnya
+        # Di sini, hanya menampilkan catatan yang telah disimpan sebelumnya sebagai contoh
+        notes = [
+            "Catatan 1",
+            "Catatan 2",
+            "Catatan 3"
+        ]
+        return [html.Li(note) for note in notes]
+    else:
+        return []
+
+# Menjalankan aplikasi
 if __name__ == '__main__':
     app.run_server(debug=True)
+
